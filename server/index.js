3c2af135a7279b0cb8e0c28b32d06c8f0297493e
Casey <caseyconlanshopping@gmail.com>
2023-06-06 11:22:21 -0700

    bay window images added

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
+fetch('https://perenual.com/docs/api')
+    .then(response => response.json())
+    .then(data => {
+        // data is the plant info. You can use it in your game.
+        console.log(data);
+    });
+
 document.addEventListener("DOMContentLoaded", function(){
     for (let i = 1; i <= 6; i++) {
         let plantElement = document.getElementById('plant' + i);
@@ -12,3 +19,24 @@ function openMenu(plantNumber) {
     // You can replace this with your actual menu opening logic.
     console.log('Opening menu for plant ' + plantNumber);
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    for (let i = 1; i <= 6; i++) {
+        let plantElement = document.getElementById('plant' + i);
+        let plantActionElement = plantElement.querySelector('.plant-action');
+        
+        plantActionElement.addEventListener('change', function(event) {
+            performAction(i, event.target.value); 
+        });
+    }
+});
+
+function performAction(plantNumber, action) {
+    console.log('Performing action ' + action + ' on plant ' + plantNumber);
+    // Add the logic for each action here.
+}
+
+let gameState = {
+    tokens: 1000,
+    plants: [null, null, null, null, null, null] // No plants at the start
+};
