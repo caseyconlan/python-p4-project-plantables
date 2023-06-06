252a505af198efec5873cea72b410f185bcd633f
Casey <caseyconlanshopping@gmail.com>
2023-06-05 19:57:10 -0700

    initial setup

--- server/models.py
+++ server/models.py
@@ -3,3 +3,15 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
