e583aba5e899a99e7129759c212ec390d6b7c9a6
Ashley King <Princessak8918@hotmail.com>
2023-06-07 14:29:04 -0600

    envpythondotenv

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -5,6 +5,8 @@ from flask import current_app
 
 from alembic import context
 
+API_KEY=sk-dfue6480c4980245d1174
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
