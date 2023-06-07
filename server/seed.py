956d1c3b2ba406cb02cf2ccb5ce813ad801e7c2d
Ashley King <Princessak8918@hotmail.com>
2023-06-07 07:48:50 -0600

    seed

--- server/seed.py
+++ server/seed.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 # Standard library imports
+from sqlalchemy import
+from sqlalchemy.orm import Session
 from random import randint, choice as rc
 
 # Remote library imports
@@ -15,3 +17,25 @@ if __name__ == '__main__':
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+import csv
+
+# Specify the number of user records you want to generate
+num_users = 100
+
+# Open the seed file in write mode
+with open('seed_file.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+
+    # Write the header row
+writer.writerow(['username', 'password', 'first_name', 'last_name', 'email','commit_message'])
+
+# Generate user data and write to the seed file
+for _ in range(num_users):
+    username = fake.user_name()
+    password = fake.password()
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    email = fake.email()
+
+    writer.writerow([username, password, first_name, last_name, email, 'Initial commit'])
+    
